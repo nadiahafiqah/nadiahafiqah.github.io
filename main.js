bcb8993bcd7ca4b73996f178243821f785a6968f
nadiahafiqah <nadiaha.ismail@gmail.com>
2023-06-10 17:24:28 +0800

    added button

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("hi!");
+const name = document.getElementById("nameInput");
+const age = document.getElementById("ageInput");
+const sex = document.getElementsByName("inlineRadio"); // need to get the value from this
+const height = document.getElementById("heightInput");
+const weight = document.getElementById("weightInput");
+const activity = document.getElementsByName("inlineRadio2"); // need to get the value from this
