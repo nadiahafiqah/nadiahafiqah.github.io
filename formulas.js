f16cce9bf0a7370b9545fd27f32109576d7e17de
nadiahafiqah <nadiaha.ismail@gmail.com>
2023-06-13 14:50:33 +0800

    macros calculation added

--- formulas.js
+++ formulas.js
@@ -23,16 +23,16 @@ const bmr = () => {
 const tdee = () => {
   let tdeeResult = 0;
   if (activity.value === "sedentary") {
-    tdeeResult = Math.round(bmr * 1.2);
+    tdeeResult = Math.round(bmr() * 1.2);
   } else if (activity.value === "lightlyactive") {
-    tdeeResult = Math.round(bmr * 1.375);
+    tdeeResult = Math.round(bmr() * 1.375);
   } else if (activity.value === "moderatelyactive") {
-    tdeeResult = Math.round(bmr * 1.55);
+    tdeeResult = Math.round(bmr() * 1.55);
   } else if (activity.value === "veryactive") {
-    tdeeResult = Math.round(bmr * 1.725);
+    tdeeResult = Math.round(bmr() * 1.725);
   } else {
     //(activity.value === "superactive")
-    tdeeResult = Math.round(bmr * 1.9);
+    tdeeResult = Math.round(bmr() * 1.9);
   }
   return tdeeResult;
 };
@@ -42,3 +42,32 @@ const tdee = () => {
 // Moderately active: moderate exercise/sports 3-5 days/week (BMR x 1.55)
 // Very active: hard exercise/sports 6-7 days/week (BMR x 1.725)
 // Extra active: very hard daily exercise/sports & physical job or 2X day training (BMR x 1.9)
+
+// Calculating macros
+// Convert weight from kg to lbs:
+const convertWeight = () => {
+  let lbs = Math.round(weight.value * 2.2046);
+  return lbs;
+};
+console.log(convertWeight());
+
+// Calculating mass of protein and fat, and remaining cals for cabrs
+let protein = Math.round(convertWeight() * 0.8);
+let fat = Math.round(convertWeight() * 0.4);
+let carbsCals = Math.round(tdee() - proteinCals() - fatCals());
+
+const proteinCals = () => {
+  let proteinCals = Math.round(protein * 4);
+  return proteinCals;
+};
+const fatCals = () => {
+  let fatCals = Math.round(fat * 9);
+  return fatCals;
+};
+const carbs = () => {
+  let intake = Math.round(carbsCals / 4);
+  return intake;
+};
+// 1g protein = 4 cals (0.8g/lb of body weight)
+// 1g fat = 9 cals (0.4g/lb of body weight)
+// 1g carbs = 4 cals (remainder cals)
